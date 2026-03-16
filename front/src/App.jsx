2401f704a34461b3b6ba9718f0494406cd66abb6
nom utilisateur <email. utilisateur>
2026-03-16 12:03:55 +0100

    maj app.jsx

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import Dashboard from './pages/Dashboard'
-import Departements from './pages/Departements'
-import DepartementDetail from './pages/DepartementDetail'
-import PlaceholderPage from './components/PlaceholderPage'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Dashboard from "./pages/Dashboard";
+import Departements from "./pages/departement";
+import DepartementDetail from "./pages/DepartementDetail";
+import PlaceholderPage from "./components/PlaceholderPage";
 
 export default function App() {
   return (
@@ -11,12 +11,18 @@ export default function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Dashboard />} />
-          <Route path="/regions" element={<PlaceholderPage title="Régions" />} />
+          <Route
+            path="/regions"
+            element={<PlaceholderPage title="Régions" />}
+          />
           <Route path="/departements" element={<Departements />} />
           <Route path="/departements/:code" element={<DepartementDetail />} />
-          <Route path="/comparateur" element={<PlaceholderPage title="Comparateur" />} />
+          <Route
+            path="/comparateur"
+            element={<PlaceholderPage title="Comparateur" />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
